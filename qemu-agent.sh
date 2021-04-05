05a7f7bf79bbcbfeb70aa9ea254b8c669af448d7
lucduke <42948608+lucduke@users.noreply.github.com>
2021-04-05 21:41:19 +0200

    Update qemu-agent.sh

--- qemu-agent.sh
+++ qemu-agent.sh
@@ -4,7 +4,7 @@
 # Rejoins moi sur Youtube: https://www.youtube.com/channel/UCsJ-FHnCEvtV4m3-nTdR5QQ
 
 # USAGE
-# https://raw.githubusercontent.com/lucduke/openmediavault/main/qemu-agent.sh | bash
+# wget -q -O - https://raw.githubusercontent.com/lucduke/openmediavault/main/qemu-agent.sh | bash
 
 # SOURCES
  
@@ -17,13 +17,21 @@ echo "Debut du script"
 echo "----------------------------------------------------------------"
 
 #1 MAJ liste des paquets 
+echo "- MAJ liste des paquets"
 apt update
 
 #2 Installation qemu-agent
+echo "- Installation qemu-agent"
 apt install -y qemu-guest-agent
 
-#3 MAJ autres paquets 
+#3 MAJ autres paquets
+echo "- MAJ autres paquets"
 apt full-upgrade -y
 
 #4 Redemarrage OMV
+echo "- Redemarrage OMV"
 reboot
+
+echo "----------------------------------------------------------------"
+echo "Fin du script"
+echo "----------------------------------------------------------------"
